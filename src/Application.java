20338a71b5773c2e94c11b96291a01c96949f422
Philipp Seitz <seitzphilipp22@gmail.com>
2018-02-07 09:19:11 +0100

    Added a findChain methode

--- src/Application.java
+++ src/Application.java
@@ -12,7 +12,12 @@ public class Application {
         ArrayList<PrimeTwins> listOfTwins = new ArrayList<>();
         listOfTwins = findPrimeTwins(getListOfPrimes());
 
-        aNewHope(listOfTwins);
+        //aNewHope(listOfTwins);
+        findChain(listOfTwins, 0);
+
+        //Ausgabe nur wenn sizeOf größer 2
+
+3
     }
 
 
@@ -21,17 +26,13 @@ public class Application {
         for (int number = 2; number <= 3000; number++) {
             if (isPrime(number)) {
                 listOfPrimes.add(number);
-        //        System.out.println(number);
             }
         }
-       // System.out.println("ende");
         return listOfPrimes;
     }
 
     public boolean isPrime(int number) {
-        //check if n is a multiple of 2
         if (number % 2 == 0) return false;
-        //if not, then just check the odds
         for (int i = 3; i * i <= number; i += 2) {
             if (number % i == 0)
                 return false;
@@ -40,7 +41,6 @@ public class Application {
     }
 
     public ArrayList<PrimeTwins> findPrimeTwins(ArrayList<Integer> list) {  //Works!
-        //prime-2 = prime
         ArrayList<PrimeTwins> primeTwinsArrayList = new ArrayList<>();
         for (int index = 0; index <= list.size() - 2; index++) {
             if (list.get(index) == list.get(index + 1) - 2) {
@@ -65,19 +65,29 @@ public class Application {
     }
 
     private void aNewHope(ArrayList<PrimeTwins> list){
-        ArrayList<ArrayList<Integer>> fuckingContainerList = new ArrayList<>();
+        ArrayList<ArrayList<PrimeTwins>> fuckingContainerList = new ArrayList<>();
         for (int x = 0; x <= list.size() - 2; x++) {
-            for (int y = x; y < list.size(); y++){ //code can be better
-                boolean a = list.get(x).getSum() == list.get(y).getMiddle();
-                if(a) {
-                    System.out.println("First : " + list.get(x).getFirst());
-                    System.out.println("Second: " + list.get(x).getSecond());
-                    System.out.println("First : " + list.get(y).getFirst());
-                    System.out.println("Second: " + list.get(y).getSecond());
-                }
-            }
-
+            findChain(list, x);
         }
+    }
+
+    public void  findChain (ArrayList<PrimeTwins> list, int index) {
+        ArrayList <Integer> chainList = new ArrayList<>();
+        chainList.add(list.get(index).getFirst());
+        chainList.add(list.get(index).getSecond());
+        int x;// = index;
+        for ( x = 0; x < list.size() ; x++) {
+            if(list.get(index).getSum() == list.get(x).getMiddle()){
+                System.out.println("Sum"+ list.get(index).getSum()  + "Middle:" + list.get(x).getMiddle());
+                System.out.println("index" + index);
+                System.out.println("x" + x);
+                index = x;
+                chainList.add(list.get(x).getFirst());
+                chainList.add(list.get(x).getSecond());
+
+            }
+        }
+        System.out.println(chainList);
     }
 
 
